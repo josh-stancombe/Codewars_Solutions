981f6066391de94ca9e46904d6e0682272e1fedf
Josh Stancombe <61159870+josh-stancombe@users.noreply.github.com>
2021-10-01 13:28:15 +0100

    Update solution.php

--- PHP/2. Find the odd int (6 kyu)/solution.php
+++ PHP/2. Find the odd int (6 kyu)/solution.php
@@ -2,26 +2,26 @@
 
 function findIt(array $seq) : int {
 
-// Loop through each item in array
-foreach($seq as $val1){
+  // Loop through each item in array
+  foreach($seq as $val1){
 
-  $count = 0;
+    $count = 0;
 
-  // Loop through array again and count when values match
-  foreach($seq as $val2){
-    if($val1 == $val2){
-      $count += 1;    
+    // Loop through array again and count when values match
+    foreach($seq as $val2){
+      if($val1 == $val2){
+        $count += 1;    
+      }
     }
-  }
 
-  // Check if count is odd
-  if($count % 2 == 0){
-    continue;
-  } else {
-    return $val1;
-  }
+    // Check if count is odd
+    if($count % 2 == 0){
+      continue;
+    } else {
+      return $val1;
+    }
 
-}
+  }
 }
 
 ?>
