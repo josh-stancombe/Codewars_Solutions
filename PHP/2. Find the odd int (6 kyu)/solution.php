ac3a2beac8b3c8f3d2d687e9e7476d54ac461291
Josh Stancombe <61159870+josh-stancombe@users.noreply.github.com>
2021-10-01 13:27:54 +0100

    Create solution.php

---
+++ PHP/2. Find the odd int (6 kyu)/solution.php
@@ -0,0 +1,27 @@
+<?PHP
+
+function findIt(array $seq) : int {
+
+// Loop through each item in array
+foreach($seq as $val1){
+
+  $count = 0;
+
+  // Loop through array again and count when values match
+  foreach($seq as $val2){
+    if($val1 == $val2){
+      $count += 1;    
+    }
+  }
+
+  // Check if count is odd
+  if($count % 2 == 0){
+    continue;
+  } else {
+    return $val1;
+  }
+
+}
+}
+
+?>
