9084bf48a91c0f98c5bf0529330891dcfb773aa9
Josh Stancombe <61159870+josh-stancombe@users.noreply.github.com>
2021-10-12 09:10:33 +0100

    Update solution.php

--- PHP/8. Which are in?/solution.php
+++ PHP/8. Which are in?/solution.php
@@ -5,29 +5,18 @@ function inArray($array1, $array2) {
   $returnArray = [];
   
   // Foreach to check if the term is in any of the words.
-  foreach($array2 as $word){
-    
+  foreach($array2 as $word){  
     foreach($array1 as $term){
-      
       if(substr_count($word, $term) == 1){
-        
-        //check if term already exists in array
-        if(in_array($term,$returnArray) == 0){ 
-          
+        if(in_array($term,$returnArray) == 0){ //check if term already exists in array
            array_push($returnArray,$term);
-          
         }
-        
       }
-      
     }
-    
   }
   
   sort($returnArray);
-  
   return $returnArray;
-  
 }
 
 ?>
